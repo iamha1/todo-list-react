e46053e6d0af83b0803db25fe3dc0db885ec207d
Ha Nguyen <nguyenthingocha2@gmail.com>
2021-03-28 18:56:02 -0400

    creating todos components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import TodoList from './TodoList';
+import uuid from 'uuid'
+
 
 function App() {
+  const [todos, setTodos] = useState([{ }])
+  const todoNameRef = useRef()
+  const { uuid } = require('uuidv4');
+
+
+  function handleAddTodo(e){
+    const name = todoNameRef.current.value
+    if (name === " ")
+    setTodos(prevTodos => {
+      return [...prevTodos, { id: uuid(), name: name, complete: false }]
+    })
+    
+    todoNameRef.current.value = null
+    
+  }
+
   return (
     <>
-    <TodoList />
-    <input type="text" />
+    <TodoList todos = {todos}/> 
+    <input ref={todoNameRef} type="text" />
+    <button onClick= {handleAddTodo}>Add Todo</button>
     <button>Add Todo</button>
     <button>Clear Completed</button>
     <div> 0 left to do</div>
-
-
 
 </>
   )
   
 }
-
-
-
-
 export default App;
---
+++ src/Todo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+export default function Todo( { todo }) {
+    return (
+        <div>
+
+            <label>
+                <input type="checkbox" checked = {todo.completed} />
+            {todo.name} 
+
+            </label>
+        </div>
+       
+    )
+}
