fd186f94217c321f21092b19c0f40f00332ea025
Ha Nguyen <nguyenthingocha2@gmail.com>
2021-03-28 16:07:40 -0400

    initial commits and delete unused files

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import TodoList from './TodoList';
+
+function App() {
+  return (
+    <>
+    <TodoList />
+    <input type="text" />
+    <button>Add Todo</button>
+    <button>Clear Completed</button>
+    <div> 0 left to do</div>
+
+
+
+</>
+  )
+  
+}
+
+
+
+
+export default App;
